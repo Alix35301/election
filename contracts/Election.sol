28906b11e9132ae9a8e32eab2137dbbe4a245d7f
Ali Hassan <Alix35301@gmail.com>
2023-11-17 16:30:12 +0500

    init

---
+++ contracts/Election.sol
@@ -0,0 +1,52 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Read/write candidates
+    mapping(uint => Candidate) public candidates;
+
+    // to keep track of people who have voted; this stores there address
+    mapping(address => bool) public voters;
+
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    constructor () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    
+    }
+
+
+    function vote (uint  _candidateId) public {
+
+        // make sure that the perosn has not voted
+        require(!voters[msg.sender]);
+
+        // ensure valid candidate Id
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record
+        voters[msg.sender] = true;
+
+        // update canidates vote
+
+        candidates[_candidateId].voteCount++;
+
+        return;
+    }
+
+
+
+}
